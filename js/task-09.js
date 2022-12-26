93375c38b1f71547dbdedd1f1468c0e690aa021f
Dariaaal <1406dariaal@gmail.com>
2022-12-26 20:42:09 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
+const colorBtn = document.querySelector('button.change-color');
+const spanTextColor = document.querySelector('span.color')
+
+colorBtn.addEventListener('click', getRandomHexColor)
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  spanTextColor.textContent = color;
+  document.body.style.backgroundColor = color;
 }
