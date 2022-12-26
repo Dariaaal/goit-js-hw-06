8cbf3e5afa87be69c6b44fac2842eb6ff1c4c9f8
Dariaaal <1406dariaal@gmail.com>
2022-12-26 21:45:36 +0200

    8?

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const user = document.querySelector('form.login-form');
+const login = document.getElementsByName("name");
+const password = document.getElementsByName("password");
 
+user.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event){
+    event.preventDefault();
+    const {elements:{login, password}} = event.currentTarget;
+    if (login.value === "" || password.value === ""){
+        console.log("Alert! Fill in all the fields");
+    } 
+    console.log(`Login: ${login.value}, Password: ${password.value}`)
+    event.currentTarget.reset();
+}
