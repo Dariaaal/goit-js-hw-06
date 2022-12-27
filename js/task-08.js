186cf733b3cf433b0d4355bb5630b9cddda83bb3
Dariaaal <1406dariaal@gmail.com>
2022-12-27 13:57:40 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,15 +1,16 @@
-const user = document.querySelector('form.login-form');
+const form = document.querySelector('form.login-form');
 const login = document.getElementsByName("name");
 const password = document.getElementsByName("password");
 
-user.addEventListener('submit', onFormSubmit);
+form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event){
     event.preventDefault();
-    const {elements:{login, password}} = event.currentTarget;
-    if (login.value === "" || password.value === ""){
-        console.log("Alert! Fill in all the fields");
+    const {elements:{email, password}} = event.currentTarget;
+    if (email.value === '' || password.value === ''){
+        return alert('Please fill in all the fields!');
     } 
-    console.log(`Login: ${login.value}, Password: ${password.value}`)
+    const userDetails = {Email: email.value, Password: password.value};
+    console.log(userDetails);
     event.currentTarget.reset();
 }
