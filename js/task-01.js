463387dde5bc4d93a44e445e126e5a6b056ab864
Dariaaal <1406dariaal@gmail.com>
2022-12-28 10:35:06 +0200

    corrected 1 task

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,20 @@
-const categories = document.querySelectorAll("#categories");
-const item = document.querySelectorAll(".item");
+const item = document.querySelectorAll("#categories");
+const categories = document.querySelectorAll(".item");
 
-console.log("Number of categories: " + item.length);
+console.log("Number of categories: ", item.length);
 
-console.log("Category: " + item[0].firstElementChild.textContent);
-console.log("Elements: " + item[0].lastElementChild.children.length);
+// console.log("Category: " + item[0].firstElementChild.textContent);
+// console.log("Elements: " + item[0].lastElementChild.children.length);
 
-console.log("Category: " + item[1].firstElementChild.textContent);
-console.log("Elements: " + item[1].lastElementChild.children.length);
+// console.log("Category: " + item[1].firstElementChild.textContent);
+// console.log("Elements: " + item[1].lastElementChild.children.length);
 
-console.log("Category: " + item[2].firstElementChild.textContent);
-console.log("Elements: " + item[2].lastElementChild.children.length);
+// console.log("Category: " + item[2].firstElementChild.textContent);
+// console.log("Elements: " + item[2].lastElementChild.children.length);
+
+categories.forEach((item)=>{
+    const title = item.firstElementChild.textContent;
+    const elements = item.lastElementChild.children.length;
+    console.log("Category: ", title);
+    console.log("Elements: ", elements);
+})
