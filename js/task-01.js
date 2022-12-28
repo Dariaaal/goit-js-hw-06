f2e5bef232c83cfd6a5a4e61ed1aee8feeacd954
Dariaaal <1406dariaal@gmail.com>
2022-12-28 10:37:07 +0200

    1

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,9 @@
-const item = document.querySelectorAll("#categories");
-const categories = document.querySelectorAll(".item");
+const categories = document.querySelectorAll("#categories");
+const items = document.querySelectorAll(".item");
 
-console.log("Number of categories: ", item.length);
+console.log("Number of categories: ", items.length);
 
-// console.log("Category: " + item[0].firstElementChild.textContent);
-// console.log("Elements: " + item[0].lastElementChild.children.length);
-
-// console.log("Category: " + item[1].firstElementChild.textContent);
-// console.log("Elements: " + item[1].lastElementChild.children.length);
-
-// console.log("Category: " + item[2].firstElementChild.textContent);
-// console.log("Elements: " + item[2].lastElementChild.children.length);
-
-categories.forEach((item)=>{
+items.forEach((item)=>{
     const title = item.firstElementChild.textContent;
     const elements = item.lastElementChild.children.length;
     console.log("Category: ", title);
